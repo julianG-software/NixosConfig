15eb717d98bb41ccdf63e1fdfae923251fab8aec
Julian Gerke <julian.gerke99@gmail.com>
2025-03-10 23:38:44 +0100

    Create Shells Module

---
+++ modules/programs/shells/zsh.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib , config, programs, ... }:{
+  imports = [
+    # Paths to other modules.
+    # Compose this module out of smaller ones.
+  ];
+
+  options = {
+    # Option declarations.
+    # Declare what settings a user of this module can set.
+    # Usually this includes a global "enable" option which defaults to false.
+
+    zsh.enable = lib.mkEnableOption "enables zsh shell";
+  };
+
+  config = lib.mkId config.zsh.enable {
+    # Option definitions.
+    # Define what other settings, services and resources should be active.
+    # Usually these depend on whether a user of this module chose to "enable" it
+    # using the "option" above. 
+    # Options for modules imported in "imports" can be set here.
+    
+    # enable the shell system wide
+    programs.zsh.enable = true;
+  };
+}
