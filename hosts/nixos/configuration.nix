52512f26b26196cd27dbc79afdd8f58085f86c62
Julian Gerke <julian.gerke99@gmail.com>
2025-03-10 15:21:30 +0100

    enable ssh

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -176,4 +176,18 @@
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
 
+  # SSH
+
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
+
 }
