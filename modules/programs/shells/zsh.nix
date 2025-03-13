c4476c2d2b4d7ae4e1610c6ab6351a2c75dfe27b
Julian Gerke <julian.gerke99@gmail.com>
2025-03-13 18:17:14 +0100

    clean up zsh module

--- modules/programs/shells/zsh.nix
+++ modules/programs/shells/zsh.nix
@@ -7,26 +7,14 @@
 }:
 {
   imports = [
-    # Paths to other modules.
-    # Compose this module out of smaller ones.
     ../../../environment/aliases/commonAliases.nix
   ];
 
   options = {
-    # Option declarations.
-    # Declare what settings a user of this module can set.
-    # Usually this includes a global "enable" option which defaults to false.
-
     zsh.enable = lib.mkEnableOption "enables zsh shell";
   };
 
   config = lib.mkIf config.zsh.enable {
-    # Option definitions.
-    # Define what other settings, services and resources should be active.
-    # Usually these depend on whether a user of this module chose to "enable" it
-    # using the "option" above.
-    # Options for modules imported in "imports" can be set here.
-
     # enable the shell system wide
     programs.zsh.enable = true;
     commonAliases.enable = true;
