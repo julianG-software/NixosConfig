4d48c3942dceddcd9e839795ec5b0ae7d6e99d58
Julian Gerke <julian.gerke99@gmail.com>
2025-03-13 12:17:31 +0100

    refacor allowd ssh keys

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -106,10 +106,10 @@
   };
 
   # Setup Allowed SSH Keys for User	
+  # note: ssh-copy-id will add user@your-machine after the public key
+  # but we can remove the "@your-machine" part
   users.users.ulianj.openssh.authorizedKeys.keys = [
-  	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgFYF8ny+v6cRWu+LeCYSGq4HkDOLpwvHUx7bALxzTH ulianj" # content of authorized_keys file
-  	# note: ssh-copy-id will add user@your-machine after the public key
-  	# but we can remove the "@your-machine" part
+  	"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgFYF8ny+v6cRWu+LeCYSGq4HkDOLpwvHUx7bALxzTH ulianj" # Gamboot "Work" Pc
   ];
 
   home-manager = {
