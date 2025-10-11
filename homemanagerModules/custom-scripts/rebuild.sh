cc1b8c08bf5ac2778d7b5acadacba51222d02154
Julian Gerke <julian.gerke99@gmail.com>
2025-10-11 16:49:34 +0200

    refactor: simplify rebuild scripts to reduce maintenance

--- homemanagerModules/custom-scripts/rebuild.sh
+++ homemanagerModules/custom-scripts/rebuild.sh
@@ -3,18 +3,4 @@
 # Get argument from command line, default to #value1 if none provided
 ARGUMENT="${1:-nixos}"
 
-
-case "$ARGUMENT" in
-        "nixos")
-                sudo nixos-rebuild switch --flake ~/.config/nixos/hosts/nixos/#nixos
-            exit 0
-        ;;
-        *) echo default
-            echo "please specify host"
-            exit 2
-        ;;
-esac
-
-
-
-
+sudo nixos-rebuild switch --flake ~/.config/nixos/hosts/$ARGUMENT
