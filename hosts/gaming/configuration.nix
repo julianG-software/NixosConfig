e7e9b377bd1e82c0baada22e694b31b12b9e775a
Julian Gerke <49532943+julianG-software@users.noreply.github.com>
2025-11-23 16:09:19 +0100

    update networking hostname

--- hosts/gaming/configuration.nix
+++ hosts/gaming/configuration.nix
@@ -38,7 +38,7 @@
     fsType = "ext4";
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-gaming"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
