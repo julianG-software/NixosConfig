d4c2277602a7dca4996ace5a6a415020d05a6589
Julian Gerke <49532943+julianG-software@users.noreply.github.com>
2025-12-07 10:46:00 +0100

    install: firefox dev edition

--- hosts/gaming/configuration.nix
+++ hosts/gaming/configuration.nix
@@ -185,6 +185,7 @@
     ethtool
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    firefox-devedition
     git
     p7zip
     gh
